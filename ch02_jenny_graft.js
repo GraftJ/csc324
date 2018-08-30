84f938d3f3ed1c7e3b1c0d2d655c12ce44735a3d
Jenny Graft <jenny.graft@gmail.com>
2018-08-29 21:06:57 -0400

    comtinued homework for ch2

--- ch02_jenny_graft.js
+++ ch02_jenny_graft.js
@@ -1,8 +1,40 @@
-console.log("Jenny Graft")
-console.log("Chapter 2 Homework \n")
+console.log("Jenny Graft");
+console.log("Chapter 2 Homework\n");
 
-console.log("Looping a Triangle \n")
+console.log("Looping a Triangle");
 
 for (let i = 1; i <= 7; i++){
     console.log("#".repeat(i));
 }
+
+console.log("\nFizzBuzz");
+console.log("Just Fizz or Buzz.")
+
+for (n = 1; n <= 100; n++){
+    if (n % 3 == 0){
+        console.log("Fizz")
+    }
+    else if (n % 5 == 0){
+        console.log("Buzz")
+    }
+    else
+    console.log(n)
+}
+
+console.log("Including FizzBuzz as well.")
+
+for (n = 1; n <= 100; n++){
+    if (n % 3 == 0){
+        console.log("Fizz")
+    }
+    if (n % 5 == 0){
+        console.log("Buzz")
+    }
+    if (n % 3 == 0 && n % 5 == 0){
+        console.log("FizzBuzz")
+    }
+    else
+    console.log(n)
+}
+
+console.log("\nUnsure how to get the output to include FizzBuzz as an answer, code not correct right now.")
