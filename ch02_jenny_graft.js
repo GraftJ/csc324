e6a5901b755f567e6129db267408252e981dec42
Jenny Graft <jenny.graft@gmail.com>
2018-09-06 14:34:43 -0400

    homework tweeks

--- ch02_jenny_graft.js
+++ ch02_jenny_graft.js
@@ -24,37 +24,37 @@ for (n = 1; n <= 100; n++) {
 
 console.log("\nIncluding FizzBuzz as well.");
 
-for (n = 1; n <= 100; n++) {
-    if (n % 3 == 0 && n % 5 !== 0) {
-        console.log("Fizz")
-    }
-    if (n % 5 == 0 && n & 3 !== 0) {
-        console.log("Buzz")
-    }
-    if (n % 3 == 0 && n % 5 == 0) {
+for (n = 1; n <= 100; n++) { 
+    if (n % 15 === 0) {
         console.log("FizzBuzz")
     }
-    if (n % 3 !== 0 && n % 5 !== 0) {
+    else if (n % 3 === 0) {
+        console.log("Fizz")
+    }
+    else if (n % 5 === 0) {
+        console.log("Buzz")
+    }
+    else {
         console.log(n)
     }
 }
 
-console.log("\nUnsure how to get the output to include FizzBuzz" +
-" as an answer, code not correct right now.\nHowever the book offers this solution:");
+console.log("\nThis code comes from Dr. Whites solution and allows what I was originally" +
+    " trying to do work.\nHowever the book offers this solution:");
 
 for (let n = 1; n <= 100; n++) {
     let output = "";
     if (n % 3 == 0) output += "Fizz";
     if (n % 5 == 0) output += "Buzz";
     console.log(output || n);
-  }
+}
 
-  console.log("\n This works because at the end of the loop it will eather print" +
-  " out the output wich can be 'Fizz,' 'Buzz,' or 'FizzBuzz' becasue the output is" + 
-  " empty unless either or both 'Fizz' or 'Buzz' is added to the empty string." +
-  " Otherwise if there is nothing in the output it will print out the number.");
+console.log("\n This works because at the end of the loop it will eather print" +
+    " out the output wich can be 'Fizz,' 'Buzz,' or 'FizzBuzz' becasue the output is" +
+    " empty unless either or both 'Fizz' or 'Buzz' is added to the empty string." +
+    " Otherwise if there is nothing in the output it will print out the number.");
 
-  console.log("\n Chessboard");
+console.log("\n Chessboard");
 
 let size = 8;
 
