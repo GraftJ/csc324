59b3bd20a30fb572dd015b016c38bdcf85a3ba42
Jenny Graft <jenny.graft@gmail.com>
2018-09-10 21:06:07 -0400

    working on ch3

--- ch03_jenny_graft.js
+++ ch03_jenny_graft.js
@@ -12,8 +12,19 @@ function min(x, y) {
 }
 
 console.log(min(0, 10));
-
 console.log(min(0, -10));
 
 console.log("\nRecursion");
+
+function isEven(n) {
+    if (n == 0) return true;
+    else if (n == 1) return false;
+    else if (n < 0) return isEven(-n);
+    else return isEven(n - 2);
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+
+console.log("\nBean Counting")
 
