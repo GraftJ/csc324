88f7108569b0db447dec1fb4cf6f66e1e54e6222
Jenny Graft <jenny.graft@gmail.com>
2018-10-02 15:22:08 -0400

    table stuff

--- table-assign/js/custom.js
+++ table-assign/js/custom.js
@@ -36,16 +36,57 @@
 
  // complete with code to select and populate the table
 
-const linkParagraph = document.querySelector("#listLinks");
-const list = document.createElement("ul");
-artists.forEach(function(artist) {
-  const listItem = document.createElement("li");
-  const anchor = document.createElement("a");
-  anchor.href = artist.link;
-  anchor.target = "_blank"
-  anchor.innerHTML = artist.name;
-  listItem.appendChild(anchor);
-  list.appendChild(listItem);
-});
+// const linkParagraph = document.querySelector("#listLinks");
+// const list = document.createElement("ul");
+// artists.forEach(function(artist) {
+//   const listItem = document.createElement("li");
+//   const anchor = document.createElement("a");
+//   anchor.href = artist.link;
+//   anchor.target = "_blank"
+//   anchor.innerHTML = artist.name;
+//   listItem.appendChild(anchor);
+//   list.appendChild(listItem);
+// });
 
-linkParagraph.appendChild(list);
+// linkParagraph.appendChild(list);
+
+//sort by name
+function byName(a, b) {
+  if (a.name < b.name) return -1;
+  if (a.name > b.name) return 1;
+  return 0;
+}
+
+//sort
+
+console.log(artists.sort(byName));
+
+//sort random
+
+function pickArtist() {
+  const randomIndex = Math.floor(Math.random() * artists.length);
+  return artists[randomIndex].name;
+}
+
+//shuffle artists randomly
+
+Array.prototype.shuffle = function() {
+  let input = this;
+  for (let i = input.length-1; 1 >= 1; i--) {
+    let randomIndex = Math.floor(Math.random() * (i + 1));
+    let itemAtIndex = input(randomIndex);
+    input[randomIndex] = input[i];
+    input[i] = itemAtIndex;
+  }
+  return imput;
+}
+
+// a button showing random artist
+
+const randomButton = document.querySelector("#random-button");
+randomButton.addEventListener("click", showRandomArtist);
+
+function showRandomArtist() {
+  const artist = document.querySelector("#random-artist");
+  artist.innerHTML = pickArtist();
+}
